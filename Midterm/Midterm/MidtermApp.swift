22632b23646cb44569961cbe9b3fe8d013b2968a
pellion24 <ashvijhosdurg@gmail.com>
2025-02-20 11:31:03 -0500

    Something

---
+++ Midterm/Midterm/MidtermApp.swift
@@ -0,0 +1,17 @@
+//
+//  MidtermApp.swift
+//  Midterm
+//
+//  Created by Ashvij Hosdurg on 2/20/25.
+//
+
+import SwiftUI
+
+@main
+struct MidtermApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Midterm/MidtermTests/MidtermTests.swift
@@ -0,0 +1,16 @@
+//
+//  MidtermTests.swift
+//  MidtermTests
+//
+//  Created by Ashvij Hosdurg on 2/20/25.
+//
+
+import Testing
+
+struct MidtermTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
