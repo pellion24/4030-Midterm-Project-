55c6b72a61d786a814e63b324b82508f5a91f479
pellion24 <ashvijhosdurg@gmail.com>
2025-02-20 12:20:40 -0500

    user input

--- Midterm/Midterm/ContentView.swift
+++ Midterm/Midterm/ContentView.swift
@@ -5,15 +5,24 @@
 //  Created by Ashvij Hosdurg on 2/20/25.
 //
 
+
 import SwiftUI
+import Combine
 
 struct ContentView: View {
+    
+    @State private var city: String = ""
+    //@FocusState private var emailFieldIsFocused: Bool = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Enter your choice of city: ")
+            TextField(
+                "city ",
+                text: $city
+            )
+
+            Text("Your city is \(city)")
         }
         .padding()
     }
